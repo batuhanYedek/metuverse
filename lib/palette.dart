9b80740b982df8501e000c6d8c08359c168f9625
BatuhanSANDIKCI <batuhansandikci@gmail.com>
2022-12-20 12:35:27 +0300

    version:1

---
+++ lib/palette.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle kHeading = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+);
+
+const TextStyle metuverseHeading_Figma = TextStyle(
+    fontFamily: 'Poppins',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w500,
+    fontSize: 28.0,
+    color: Color(0xFFFFFFFF),
+);
+
--- lib/screens/login-page.dart
+++ lib/screens/login-page.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:metuverse/palette.dart';
+import '../widgets/widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-          decoration: const BoxDecoration(image: DecorationImage(
-              image: AssetImage('assets-images/metuverse_login_screen.png'),
-              fit: BoxFit.fill,
-              //colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
-          ),
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  height: 460,
+                  child: Center(
+                    child: Text(
+                      'Metuverse',
+                      style: kHeading,
+                    ),
+                  ),
+                ),
+
+
+
+              ],
+            ),
+          )
         ),
+      ],
     );
   }
 }
+
